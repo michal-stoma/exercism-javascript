95de5bab801f8be33d82fc0f8ed767860b25e030
Michal Stoma <michal.stoma@tivo.com>
2019-07-26 12:33:48 +0200

    Resistor Color Duo: add some documentation to reducer function.

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -10,8 +10,19 @@ const COLORS = ["black",
 				"white"
 				];
 
-// reducer reduces Array of digits into an integer based on radix 10
-const reducer = (acc, cur, idx, src) => acc += cur * 10 ** (src.length - idx - 1);
+function reducer (acc, cur, idx, src) {
+	/**
+	 * Reducer function to reduce Array of digits into an radix 10 based integer
+	 * Usage:
+	 * [4,3,1].reduce(reducer, 0). This will return: 431
+	 * idx starts with 0 when initial value is used.
+	 * (src.length - idx - 1) calculates power value for a digit base on it's position.
+	 * For example: for digit 4 from [4,3,2] src.length is 3, idx is 0,
+	 * therefore its power is 2 representing value of hundreds in the output.
+	 **/
+
+	return acc += cur * 10 ** (src.length - idx - 1);
+}
 
 export const value = (colorsList) => colorsList
 										.map(it => COLORS.indexOf(it))
